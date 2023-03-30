e4907265e62d044fbacb80ac5abff98af0e5f644
Joe <joe@lowerrocklabs.com>
2023-03-30 01:51:50 +0000

    Test

---
+++ tests copy/Standard/Models/Species.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Tests\Standard\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Species extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'species';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function pets(): HasMany
+    {
+        return $this->hasMany(Pet::class);
+    }
+}
