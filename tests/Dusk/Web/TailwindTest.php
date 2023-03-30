3843581e29fe2456a7b35ccc944d61791fcff8b4
Joe <joe@lowerrocklabs.com>
2023-03-30 02:41:24 +0000

    Test

--- tests/Dusk/Web/TailwindTest.php
+++ tests/Dusk/Web/TailwindTest.php
@@ -19,7 +19,7 @@ class TailwindTest extends DuskTestCase
 
             $browser->pause(1000);
 
-            $browser->press('div.flex-col > div.md\:flex.md\:justify-between.mb-4.px-4.md\:p-0 > div.w-full.mb-4.md\:mb-0.md\:w-2\/4.md\:flex.space-y-4.md\:space-y-0.md\:space-x-2 > div.relative.block.md\:inline-block.text-left > div:nth-child(1) > button');
+            $browser->press('@filtBtn');
 
             $browser->pause(1000);
 
