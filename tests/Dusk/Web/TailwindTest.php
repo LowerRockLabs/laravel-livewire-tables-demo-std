3e24b8f0497aa42b4b586f63c04f0ff643dc9a78
Joe <joe@lowerrocklabs.com>
2023-03-30 01:30:34 +0000

    Tests 1

---
+++ tests/Dusk/Web/TailwindTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Tests\Dusk\Web;
+
+use Laravel\Dusk\Browser;
+use Rappasoft\LaravelLivewireTables\Tests\Dusk\DuskTestCase;
+
+class TailwindTest extends DuskTestCase
+{
+    /**
+     * All Filters Load
+     */
+    public function testFilterMenuOpensAll(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tailwind');
+
+            $browser->assertDontSee('E-mail Verified');
+
+            $browser->pause(1000);
+
+            $browser->press('@filtBtn');
+
+            $browser->pause(1000);
+
+            $browser->assertSee('E-mail Verified');
+        });
+    }
+}
